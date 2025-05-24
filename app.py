e65d033453455a5659fba373709d291cb4680d48
Vedant <vedantchavanjolly@gmail.com>
2025-05-24 18:25:49 +0530

    Updated app.py with URL shortening functionality and made other changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template_string
+from flask import Flask, request, redirect, render_template_string, render_template
 import hashlib
 
 app = Flask(__name__)
@@ -7,25 +7,17 @@ app = Flask(__name__)
 url_map = {}
 
 # Route for the homepage (index)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template_string("""
-        <h1>URL Shortener</h1>
-        <form action="/shorten" method="post">
-            <label for="url">Enter URL to shorten:</label>
-            <input type="text" id="url" name="url" required>
-            <input type="submit" value="Shorten">
-        </form>
-    """)
-
-# Route to handle URL shortening
-@app.route('/shorten', methods=['POST'])
-def shorten():
-    original_url = request.form['url']
-    # Create a simple hash of the URL to shorten it
-    shortened_url = hashlib.md5(original_url.encode()).hexdigest()[:6]
-    url_map[shortened_url] = original_url
-    return f"Shortened URL: <a href='/{shortened_url}'>/{shortened_url}</a>"
+    short_url = None
+    if request.method == 'POST':
+        original_url = request.form['url']
+        # Create a simple hash of the URL to shorten it
+        shortened_url = hashlib.md5(original_url.encode()).hexdigest()[:6]
+        url_map[shortened_url] = original_url
+        short_url = f"/{shortened_url}"
+    
+    return render_template('index.html', short_url=short_url)
 
 # Route to redirect to the original URL using the shortened URL
 @app.route('/<shortened_url>')
